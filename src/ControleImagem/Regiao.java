2459e60911fb95142a84be6879c39ba2dab07161
LeandroRM <leandroramosmarcelino@hotmail.com>
2017-05-02 23:46:30 -0300

    Criado metodos isPixelInRegiao(), getArea(), addPixel() para classe Regiao

--- src/ControleImagem/Regiao.java
+++ src/ControleImagem/Regiao.java
@@ -5,14 +5,36 @@ package ControleImagem;
  * @author Leandro Ramos<leandroramosmarcelino@hotmail.com>
  */
 public class Regiao extends Cor {
-    private final Cor[][] pixels;
+    private final boolean[][] pixels;
     private int media;
     
-    public Regiao(int media, Imagem imagem) {
-        super(media, media, media);
-        pixels = new Cor[imagem.getAltura()][imagem.getLargura()];
+    /**
+     *  Cria uma região de uma imagem, ou seja, a fusão de pixels de determinada imagem
+     * 
+     * @param imagem - imagem onde a região se encontra
+     * @param row1 - linha do primeiro pixel
+     * @param col1 - coluna do primeiro pixel
+     * @param row2 - linha do segundo pixel
+     * @param col2  - coluna do segundo pixel
+     */
+    public Regiao(Imagem imagem, int row1, int col1, int row2, int col2) {
+        //Define qualquer valor
+        super(0, 0, 0);
+        //Cria matriz espelho da imagem
+        
+        pixels = new boolean[imagem.getAltura()][imagem.getLargura()];
+        //Seta todos os valores como false
+        populaPixels();
+        
+        //Seta como true as duas posições pertencentes a regiao
+        pixels[row1][col1] = true;
+        pixels[row2][col2] = true;
     }
 
+    public void setMedia(int media) {
+        this.media = media;
+    }
+    
     @Override
     public int getAVG() {
         return this.media;
@@ -26,7 +48,43 @@ public class Regiao extends Cor {
      * @param col 
      */
     public void addPixel(int media, int row, int col) {
-       int novaMedia = (this.getAVG() + media) / 2;
+       setMedia((this.getAVG() + media) / 2);
+       this.pixels[row][col] = true;
+    }
+    
+    /**
+     * Retorna se o pixel daquela posição pertence a região
+     * 
+     * @param row - linha
+     * @param col - coluna
+     * @return 
+     */
+    public boolean isPixelInRegiao(int row, int col) {
+        return this.pixels[row][col];
+    }
+    
+    /**
+     * Retorna a quantidade de pixels contida na Regiao
+     * @return - Area da regiao
+     */
+    public int getArea() {
+        int count = 0;
+        for (boolean[] linha : this.pixels) {
+            for (int i = 0; i < linha.length; i++) {
+                if(linha[i]) {
+                    count++;
+                }
+            }
+        }
         
+        return count;
+    }
+    
+    private void populaPixels() {
+        for (boolean[] pixel : this.pixels) {
+            for (int j = 0; j < this.pixels[0].length; j++) {
+                pixel[j] = false;
+            }
+        }
     }
 }
